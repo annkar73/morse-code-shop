ab1d7554a17d2fcdf72f9fb21e13b2c5abfe5f59
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2025-05-05 17:20:06 +0200

    add object to array

--- src/app/data/products.ts
+++ src/app/data/products.ts
@@ -3,6 +3,7 @@
 import { IProduct } from "@/components/ProductCard"; // importera IProduct om du inte redan gör det
 
 export const products: IProduct[] = [
+  { id: 0, name: "Suck It Up", description: "", price: 79, image: "/images/bracelets.jpeg" },
   { id: 1, name: "Fuck Off", description: "", price: 79, image: "/images/bracelets.jpeg" },
   { id: 2, name: "Shut Up", description: "", price: 79, image: "/images/bracelets.jpeg" },
   { id: 3, name: "Kiss My Ass", description: "", price: 79, image: "/images/bracelets.jpeg" },
@@ -12,13 +13,13 @@ export const products: IProduct[] = [
   { id: 7, name: "Oh Fuck It", description: "", price: 79, image: "/images/bracelets.jpeg" },
   { id: 8, name: "Bad Ass", description: "", price: 79, image: "/images/bracelets.jpeg" },
   { id: 10, name: "Kuken", description: "", price: 79, image: "/images/bracelets.jpeg" },
-  { id: 11, name: "Shut Up", description: "", price: 79, image: "/images/bracelets.jpeg" },
+  { id: 11, name: "Shut The Fuck Up", description: "", price: 79, image: "/images/bracelets.jpeg" },
   { id: 12, name: "Do Epic Shit", description: "", price: 79, image: "/images/bracelets.jpeg" },
   { id: 13, name: "Y'all Need Jesus", description: "", price: 79, image: "/images/bracelets.jpeg" },
   { id: 14, name: "I Like Boys", description: "", price: 79, image: "/images/bracelets.jpeg" },
   { id: 15, name: "I Like Girls", description: "", price: 79, image: "/images/bracelets.jpeg" },
   { id: 16, name: "Proud Gay", description: "", price: 79, image: "/images/bracelets.jpeg" },
   { id: 17, name: "Proud Trans", description: "", price: 79, image: "/images/bracelets.jpeg" },
-
+  { id: 18, name: "Din Mamma", description: "", price: 79, image: "/images/bracelets.jpeg" },
 
 ];
