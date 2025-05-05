5358ff889c8c32bc7b69e3d595a771dd46d61cf2
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2025-05-05 11:50:59 +0200

    update prices

--- src/app/data/products.ts
+++ src/app/data/products.ts
@@ -3,23 +3,22 @@
 import { IProduct } from "@/components/ProductCard"; // importera IProduct om du inte redan gör det
 
 export const products: IProduct[] = [
-  { id: 1, name: "Fuck Off", description: "", price: 299, image: "/images/bracelets.jpeg" },
+  { id: 1, name: "Fuck Off", description: "", price: 99, image: "/images/bracelets.jpeg" },
   { id: 2, name: "Shut Up", description: "", price: 99, image: "/images/bracelets.jpeg" },
-  { id: 3, name: "Kiss My Ass", description: "", price: 59, image: "/images/bracelets.jpeg" },
-  { id: 4, name: "Screw You", description: "", price: 129, image: "/images/bracelets.jpeg" },
-  { id: 5, name: "Zero Fucks Given", description: "", price: 129, image: "/images/bracelets.jpeg" },
-  { id: 6, name: "Fuck It", description: "", price: 129, image: "/images/bracelets.jpeg" },
-  { id: 7, name: "Oh Fuck It", description: "", price: 129, image: "/images/bracelets.jpeg" },
-  { id: 8, name: "Bad Ass", description: "", price: 129, image: "/images/bracelets.jpeg" },
-  { id: 9, name: "Din Mamma", description: "", price: 129, image: "/images/bracelets.jpeg" },
+  { id: 3, name: "Kiss My Ass", description: "", price: 99, image: "/images/bracelets.jpeg" },
+  { id: 4, name: "Screw You", description: "", price: 99, image: "/images/bracelets.jpeg" },
+  { id: 5, name: "Zero Fucks Given", description: "", price: 99, image: "/images/bracelets.jpeg" },
+  { id: 6, name: "Fuck It", description: "", price: 99, image: "/images/bracelets.jpeg" },
+  { id: 7, name: "Oh Fuck It", description: "", price: 99, image: "/images/bracelets.jpeg" },
+  { id: 8, name: "Bad Ass", description: "", price: 99, image: "/images/bracelets.jpeg" },
   { id: 10, name: "Kuken", description: "", price: 129, image: "/images/bracelets.jpeg" },
-  { id: 11, name: "Shut Up", description: "", price: 129, image: "/images/bracelets.jpeg" },
-  { id: 12, name: "Do Epic Shit", description: "", price: 129, image: "/images/bracelets.jpeg" },
-  { id: 13, name: "Yall Need Jesus", description: "", price: 129, image: "/images/bracelets.jpeg" },
-  { id: 14, name: "I Like Boys", description: "", price: 129, image: "/images/bracelets.jpeg" },
-  { id: 15, name: "I Like Girls", description: "", price: 129, image: "/images/bracelets.jpeg" },
-  { id: 16, name: "Proud Gay", description: "", price: 129, image: "/images/bracelets.jpeg" },
-  { id: 17, name: "Proud Trans", description: "", price: 129, image: "/images/bracelets.jpeg" },
+  { id: 11, name: "Shut Up", description: "", price: 99, image: "/images/bracelets.jpeg" },
+  { id: 12, name: "Do Epic Shit", description: "", price: 99, image: "/images/bracelets.jpeg" },
+  { id: 13, name: "Yall Need Jesus", description: "", price: 99, image: "/images/bracelets.jpeg" },
+  { id: 14, name: "I Like Boys", description: "", price: 99, image: "/images/bracelets.jpeg" },
+  { id: 15, name: "I Like Girls", description: "", price: 99, image: "/images/bracelets.jpeg" },
+  { id: 16, name: "Proud Gay", description: "", price: 99, image: "/images/bracelets.jpeg" },
+  { id: 17, name: "Proud Trans", description: "", price: 99, image: "/images/bracelets.jpeg" },
 
 
 ];
